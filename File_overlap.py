25927db16dfbee9be34af658f3ae8c49f24567bc
Deepu14 <srisivanicharan1@gmail.com>
2017-07-27 01:31:33 -0700

    Update File_overlap.py

--- File_overlap.py
+++ File_overlap.py
@@ -2,28 +2,6 @@
 One .txt file has a list of all prime numbers under 1000, and the other .txt file has a list of 
 happy numbers up to 1000."""
 
-"""f1 = open("primenumbers.txt","r")
-line1 = f1.readlines()
-f2 = open("happynumbers.txt","r")
-line2 = f2.readlines()
-list1=[]
-list2=[]
-list3=[]
-for i in line1:
-    i = i.strip("\n")
-    list1.append(int(i))
-print(list1)
-
-for i in line2:
-    i = i.strip("\n")
-    list2.append(int(i))
-print(list2)
-
-for i in list1:
-    if i in list2:
-        list3.append(i)
-print(list3)"""
-
 def openfiles(filename):
     list1=[]
     f = open(filename,"r")
